1d6a3385843248d66f7f3a910df00d374ba778e1
huangyifei <903061211@qq.com>
2023-06-28 21:44:38 +0800

    登陆简单样式

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,38 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 
-const routes = [{
-    path: '/',
-    component: () => import('../views/Main.vue'),
-    redirect:'/home',
-    children: [
-        {
-            path: '/home',
-            name: 'home',
-            component: () => import('../views/home/Home.vue'),
-        },
-        {
-            path: '/user',
-            name: 'user',
-            component: () => import('../views/user/User.vue'),
-        },
-        {
-            path: '/page1',
-            name: 'page1',
-            component: () => import('../views/Page1.vue'),
-        },
-        {
-            path: '/page2',
-            name: 'page2',
-            component: () => import('../views/Page2.vue'),
-        }
-    ]
-}]
+const routes = [
+    {
+        path: '/',
+        component: () => import('../views/Main.vue'),
+        redirect: '/home',
+        children: [
+            {
+                path: '/home',
+                name: 'home',
+                component: () => import('../views/home/Home.vue'),
+            },
+            {
+                path: '/user',
+                name: 'user',
+                component: () => import('../views/user/User.vue'),
+            },
+            {
+                path: '/page1',
+                name: 'page1',
+                component: () => import('../views/Page1.vue'),
+            },
+            {
+                path: '/page2',
+                name: 'page2',
+                component: () => import('../views/Page2.vue'),
+            }
+        ]
+    },
+    {
+        path: '/login',
+        component: () => import('../views/Login.vue'),
+    }
+]
 
 const router = createRouter({
     history: createWebHashHistory(),
---
+++ src/views/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-form :model="loginForm" class="login-container">
+    <h3>系统登陆</h3>
+    <el-form-item>
+      <el-input type="input" placeholder="请输入账号"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-input type="password" placeholder="请输入密码"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" >
+        登陆
+      </el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script setup >
+
+</script>
+
+<style lang="less" scoped>
+.login-container{
+  width: 350px;
+  background-color: #fff;
+  border: 1px solid #eaeaea;
+  border-radius: 15px;
+  padding: 35px 35px 15px 35px;
+  box-shadow: 0 0 25px #cacaca;
+  margin: 180px auto;
+  h3{
+    text-align: center;
+    margin-bottom: 20px;
+    color: #505450;
+  }
+  .el-form-item_content{
+    justify-content: center;
+  }
+}
+</style>
